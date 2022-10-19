46fbc6e0799365c81ebfb46ff6839ab0e93876ba
moxey.eth <jakemoxey@gmail.com>
2022-10-19 13:20:58 +1100

    Initial commit from create-wagmi

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import { useAccount } from 'wagmi'
+
+import { Account, Connect, NetworkSwitcher } from '../components'
+
+function Page() {
+  const { isConnected } = useAccount()
+
+  return (
+    <>
+      <h1>wagmi + Next.js</h1>
+
+      <Connect />
+
+      {isConnected && (
+        <>
+          <Account />
+          <NetworkSwitcher />
+        </>
+      )}
+    </>
+  )
+}
+
+export default Page
